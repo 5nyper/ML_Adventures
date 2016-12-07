01ccb35e2d53d2bd288d4a468b72cfcb8af51435
Karim Ellaisy <karim@icomeupmarketing.com>
2016-12-06 23:45:52 -0500

    played a bit more

--- stdvar.py
+++ stdvar.py
@@ -1,25 +1,31 @@
-#Roughly predict the amount of likes on a given day when posting
-
+%matplotlib inline
 import matplotlib.pyplot as plt
 import json
 from numpy.random import rand
-from pprint import pprint
+from operator import itemgetter
+from scipy import stats
 
 with open('test.json') as data_file:
     data = json.load(data_file)
 pics = data["data"]
 month = []
 likes = []
+likti = []
 days = []
 time = []
 for member in pics:
     month.append(member["month"])
     if member["day"] == 0:
         likes.append(member["likes"])
+        likti.append([member["likes"],member["time"]])
+        time.append(member["time"])
     days.append(member["day"])
-    time.append(member["time"])
+    #time.append(member["time"])
 likes = np.array(likes)
+a = []
+newnew = np.array(sorted(likti, key=itemgetter(0), reverse=True))
+for item in newnew[:6]:
+    a.append(item[1])
 plt.hist(likes)
 plt.show()
-pprint(likes)
-likes.mean() + (likes.std() / 2)
+print(likes.mean() + (likes.std() / 2),"on", np.mean(a))
